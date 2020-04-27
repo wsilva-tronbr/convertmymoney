2abc58f4381d61fe0f601ec2bde857c86b69f60c
wsilva-tronbr <30978616+wsilva-tronbr@users.noreply.github.com>
2020-04-27 14:38:25 -0300

    Convertendo...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const convert = require('./lib/convert')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,'views'))
@@ -10,7 +11,20 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 app.get('/cotacao', (req, res) => {
-    res.render('cotacao')
+    const { cotacao, quantidade } = req.query
+    if(cotacao && quantidade){
+        const conversao = convert.convert(cotacao, quantidade)
+        res.render('cotacao', {
+            error: false,
+            cotacao: convert.toMoney(cotacao),
+            quantidade: convert.toMoney(quantidade),
+            conversao: convert.toMoney(conversao)
+        })
+    }else{
+        res.render('cotacao', {
+            error: 'Valores inválidos'
+        })
+    }
 })
 
 app.listen('3000', err => {
